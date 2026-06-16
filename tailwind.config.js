d637c1c75378c36787b6585798d55b302e9537bf
ujwaltiwari04 <ujjwaltiwari2014@gmail.com>
2026-06-16 19:11:22 +0530

    perf: compile tailwind css, optimize images to webp, and minify assets for faster load speeds

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./jobs.html",
+    "./js/**/*.js"
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif']
+      },
+      colors: {
+        brand: {
+          cyan: '#0891b2',
+          cyandark: '#0e7490',
+          blue: '#1e3a5f',
+          bluemid: '#1d4ed8',
+          light: '#e0f2fe',
+          accent: '#06b6d4',
+        }
+      }
+    },
+  },
+  plugins: [],
+}
